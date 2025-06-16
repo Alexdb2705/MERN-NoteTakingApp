9a03a0ae396e40147835cca036741cc707ea3372
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-16 22:47:30 +0200

    Added some http methods

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,18 @@ app.get("/api/notes", (req, res) => {
     res.send("you got 10 notes");
 });
 
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({ message: "Note created succesfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Note updated succesfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Note deleted succesfully!" });
+});
+
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001")
+    console.log("Server started on PORT: 5001");
 });
