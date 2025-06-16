76e9204bf436793b95cfc2871a9f4631215de67a
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-16 22:36:12 +0200

    nodemon functionality installed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {})
+app.get("/api/notes", (req, res) => {
+    // send the notes
+    res.send("you got 5 notes");
+});
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001")
