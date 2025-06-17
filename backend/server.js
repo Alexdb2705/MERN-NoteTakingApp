69e12c4c99e78e794bd92499efe53836a47ef152
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-17 18:31:27 +0200

    Routing is now in another file and folder for routes

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,11 @@
 import express from "express";
 // const express = require("express");  This line is needed instead of the top one if "type" in package.json is commonjs instead of module.
+import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    // send the notes
-    res.send("you got 10 notes");
-});
+app.use("/api/notes", notesRoutes)
 
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({ message: "Note created succesfully!" });
-});
-
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({ message: "Note updated succesfully!" });
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({ message: "Note deleted succesfully!" });
-});
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
