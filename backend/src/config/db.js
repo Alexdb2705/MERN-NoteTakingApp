1570bc930a34635fcc28f7b707b71cca6536115a
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-17 19:55:24 +0200

    Import vars moved to env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://alejandrodelbarrioagudo:hWFXa0I6HuMKJVGi@cluster0.48eshpu.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED SUCCESFULLY! ");
   } catch (error) {
     console.error("Error connecting to MONGODB", error);
