bdc6fd62082c6d61597b30699bd9a29e08011ccb
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-17 18:40:47 +0200

    Controllers moved to a separated folder and file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,21 +1,14 @@
 import express from "express"
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController";
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.status(200).send("You just fetched the notes");
-});
+router.get("/", getAllNotes);
 
-router.post("/", (req, res) => {
-    res.status(201).json({ message: "Note created succesfully!" });
-});
+router.post("/", createNote);
 
-router.put("/:id", (req, res) => {
-    res.status(200).json({ message: "Note updated succesfully!" });
-});
+router.put("/:id", updateNote);
 
-router.delete("/:id", (req, res) => {
-    res.status(200).json({ message: "Note deleted succesfully!" });
-});
+router.delete("/:id", deleteNote);
 
 export default router
