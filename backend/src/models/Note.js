21c9780e4b9462054c57b3085465fa7f0249842f
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-21 19:58:57 +0200

    Schema updated with timestamps saving

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 // 1- create a schema
 // 2- model based off of that schema
 
-const noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
